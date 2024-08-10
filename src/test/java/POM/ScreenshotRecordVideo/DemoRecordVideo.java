de129a84299be22128a2b25dadcc8733fc739382
phuongvt26 <phuongfighting1998@gmail.com>
2024-08-10 14:48:43 +0700

    SeleniumPomParallel2

--- src/test/java/POM/ScreenshotRecordVideo/DemoRecordVideo.java
+++ src/test/java/POM/ScreenshotRecordVideo/DemoRecordVideo.java
@@ -13,6 +13,7 @@ public class DemoRecordVideo extends BaseSetup {
     @Test
     public void testHomePage1() {
         CaptureHelper.startRecord("Record testHomePage1");
+
         WebUI.openURL(DataConfig.URL);
         LoginPage loginPage = new LoginPage();
         loginPage.loginCRM(DataConfig.EMAIL, DataConfig.PASSWORD);
